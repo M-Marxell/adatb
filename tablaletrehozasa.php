a9f331a2d1931cc4000b6c0fd30eff03ac2aeb17
M-Marxell <marcellmoravetz@gmail.com>
2025-03-25 08:40:28 +0100

    Táblalétrehozás teszt előtt

--- tablaletrehozasa.php
+++ tablaletrehozasa.php
@@ -12,3 +12,30 @@ if ($conn->connect_error)
 else{
     echo "Csatlakozás sikeres";
 }
+
+$sql= "DROP TABLE IF EXIST emberek";
+
+if($conn -> query($sql)===true)
+{
+    echo "Sikeres táblatörlés";
+}
+else{
+    echo "Nem sikerülta tábla törlés";
+}
+
+$sql ="CREATE TABLE IF NOT EXISTS emberek(
+id int unsigned AUTO_INCREMENT PRIMARY KEY,
+vezeteknev varchar(30) not null,
+keresztnev varchar(30) not null,
+beosztas varchar(50)not null,
+regiszt_datum timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP)
+";
+if($conn -> query($sql)===true)
+{
+    echo "Sikeres tábla létrehozás";
+}
+else{
+    echo "Nem sikerülta tábla létrehozás";
+}
+
+$conn ->close();
